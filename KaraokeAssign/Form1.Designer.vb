70dacfd207f08dccdcfb3c015e6d8fbf198cd04f
Promethibot <paola.mancini@me.com>
2023-04-04 23:08:45 -0400

    Add project files.

---
+++ KaraokeAssign/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.cbxList = New System.Windows.Forms.ComboBox()
+        Me.tbxInput = New System.Windows.Forms.TextBox()
+        Me.btnCost = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblCost = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-3, -9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(373, 462)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Freestyle Script", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.Label1.Location = New System.Drawing.Point(434, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(312, 58)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Karaoke Music Night"
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.AutoSize = True
+        Me.lblDisplay.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDisplay.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.lblDisplay.Location = New System.Drawing.Point(455, 189)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(281, 18)
+        Me.lblDisplay.TabIndex = 2
+        Me.lblDisplay.Text = "Hourly rental of Karaoke room:"
+        Me.lblDisplay.Visible = False
+        '
+        'cbxList
+        '
+        Me.cbxList.BackColor = System.Drawing.Color.FromArgb(CType(CType(52, Byte), Integer), CType(CType(52, Byte), Integer), CType(CType(52, Byte), Integer))
+        Me.cbxList.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cbxList.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.cbxList.Font = New System.Drawing.Font("Verdana", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbxList.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.cbxList.FormattingEnabled = True
+        Me.cbxList.Items.AddRange(New Object() {"Number of Songs ($2.99 per song)", "Hours in Private Karaoke Booth ($8.99 per hour)"})
+        Me.cbxList.Location = New System.Drawing.Point(444, 122)
+        Me.cbxList.Name = "cbxList"
+        Me.cbxList.Size = New System.Drawing.Size(302, 26)
+        Me.cbxList.TabIndex = 3
+        Me.cbxList.Text = "Customize your night with..."
+        '
+        'tbxInput
+        '
+        Me.tbxInput.BackColor = System.Drawing.Color.FromArgb(CType(CType(52, Byte), Integer), CType(CType(52, Byte), Integer), CType(CType(52, Byte), Integer))
+        Me.tbxInput.Font = New System.Drawing.Font("Verdana", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbxInput.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.tbxInput.Location = New System.Drawing.Point(542, 229)
+        Me.tbxInput.Name = "tbxInput"
+        Me.tbxInput.Size = New System.Drawing.Size(100, 26)
+        Me.tbxInput.TabIndex = 4
+        Me.tbxInput.Visible = False
+        '
+        'btnCost
+        '
+        Me.btnCost.BackColor = System.Drawing.Color.White
+        Me.btnCost.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCost.Enabled = False
+        Me.btnCost.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCost.ForeColor = System.Drawing.Color.FromArgb(CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer))
+        Me.btnCost.Location = New System.Drawing.Point(444, 291)
+        Me.btnCost.Name = "btnCost"
+        Me.btnCost.Size = New System.Drawing.Size(110, 25)
+        Me.btnCost.TabIndex = 5
+        Me.btnCost.Text = "Total Cost"
+        Me.btnCost.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.White
+        Me.btnClear.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnClear.Enabled = False
+        Me.btnClear.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.FromArgb(CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer))
+        Me.btnClear.Location = New System.Drawing.Point(626, 291)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(110, 25)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'lblCost
+        '
+        Me.lblCost.AutoSize = True
+        Me.lblCost.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCost.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.lblCost.Location = New System.Drawing.Point(441, 361)
+        Me.lblCost.Name = "lblCost"
+        Me.lblCost.Size = New System.Drawing.Size(103, 18)
+        Me.lblCost.TabIndex = 7
+        Me.lblCost.Text = "Total Cost:"
+        Me.lblCost.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblCost)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCost)
+        Me.Controls.Add(Me.tbxInput)
+        Me.Controls.Add(Me.cbxList)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Karaoke Music Night"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblDisplay As Label
+    Friend WithEvents cbxList As ComboBox
+    Friend WithEvents tbxInput As TextBox
+    Friend WithEvents btnCost As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lblCost As Label
+End Class
---
+++ KaraokeAssign/Form1.vb
@@ -0,0 +1,106 @@
+﻿Public Class Form1
+    Private _decSong As Decimal = 2.99D
+    Private _decRoom As Decimal = 8.99D
+
+    Private Function ValidateInput() As Boolean
+        Dim intNumber As Integer
+        Dim blnValid As Boolean = False
+
+        Try
+            intNumber = Convert.ToInt32(tbxInput.Text)
+
+            If intNumber > 0D Then
+                blnValid = True
+                Return blnValid
+            End If
+
+        Catch Exception As FormatException
+            MsgBox("Please enter a valid amount.", , "Real Numbers, Please!")
+        Catch Exception As OverflowException
+            MsgBox("Please enter a reasonable amount.", , "What are you doing?")
+        Catch Exception As SystemException
+            MsgBox("Sorry, that doesn't seem right. Please enter a valid value.", , "?!")
+        End Try
+        tbxInput.Focus()
+        tbxInput.Clear()
+
+        Return blnValid
+    End Function
+
+    Private Function RetSongCost(ByVal intpass As Integer) As Decimal
+        Dim decPassCost As Decimal
+        decPassCost = intpass * _decSong
+
+        Return decPassCost
+    End Function
+
+    Private Function RetRoomCost(ByVal intticket As Integer) As Decimal
+        Dim decTicketCost As Decimal
+        decTicketCost = intticket * _decRoom
+
+        Return decTicketCost
+    End Function
+
+    Private Sub cbxList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxList.SelectedIndexChanged
+        If (cbxList.SelectedIndex = 0) Then
+            lblDisplay.Text = "     Number of Karaoke songs:"
+            lblDisplay.Visible = True
+            tbxInput.Visible = True
+
+            btnClear.Enabled = True
+            tbxInput.Focus()
+        Else
+            lblDisplay.Text = "Hourly rental of Karaoke Room:"
+            lblDisplay.Visible = True
+            tbxInput.Visible = True
+
+            btnClear.Enabled = True
+            tbxInput.Focus()
+        End If
+    End Sub
+
+    Private Sub ClearForm()
+        lblDisplay.Visible = False
+        lblCost.Visible = False
+        btnCost.Enabled = False
+        tbxInput.Visible = False
+        tbxInput.Clear()
+        cbxList.Text = "Customize your night with..."
+        btnClear.Enabled = False
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ClearForm()
+    End Sub
+
+    Private Sub tbxInput_TextChanged(sender As Object, e As EventArgs) Handles tbxInput.TextChanged
+        If (tbxInput.TextLength > 0) Then
+            btnCost.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ClearForm()
+    End Sub
+
+    Private Sub btnCost_Click(sender As Object, e As EventArgs) Handles btnCost.Click
+        Dim blnAmountValid = False
+        Dim intValue As Integer
+        Dim decTotal As Decimal
+
+        blnAmountValid = ValidateInput()
+
+        If blnAmountValid Then
+            intValue = Convert.ToInt32(tbxInput.Text)
+            If (cbxList.SelectedIndex = 0) Then
+                decTotal = RetSongCost(intValue)
+            Else
+                decTotal = RetRoomCost(intValue)
+            End If
+
+            lblCost.Text = "Total Cost: $" + decTotal.ToString("N2")
+            lblCost.Visible = True
+        End If
+
+    End Sub
+End Class
